3c9c55369f798a0d4ba89d7c1e2bd6cbf6c25c62
Kaja <ayreeve@wp.pl>
2017-05-10 12:39:00 +0200

    iteration update

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,8 @@
 
 var button = document.getElementsByClassName('button');
 
-for (var i = 1; i <= button.length; i++) {
-    alert(button[0].innerText);
-    alert(button[1].innerText);
-    alert(button[2].innerText);
-    break
+for (var i = 0; i < button.length; i++) {
+    alert(button[i].innerText);
 }
 
 /* events */
